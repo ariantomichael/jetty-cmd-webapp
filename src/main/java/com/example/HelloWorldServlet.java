e01106c049c40f8f0e7a4119de199d1ea8ca4ff4
Research <Research>
2018-06-24 20:31:32 +0900

    2nd commit, done implementing with Nuprocess, error message still manual

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -1,37 +1,42 @@
 package com.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.zaxxer.nuprocess.NuProcess;
+import com.zaxxer.nuprocess.NuProcessBuilder;
+
 @WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet
 {
-  @Override 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException
-  { 
-      response.getOutputStream().println("<!DOCTYPE html><html><body><form action=\"/\" id=\"cmdform\" method=\"POST\">Command: <input type=\"text\" name=\"cmd\"><input type=\"submit\" value=\"Execute\"></form></body></html>");
-  }
-  @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) 
-    throws IOException {
-        String command = request.getParameter("cmd");
-        ProcessBuilder builder = new ProcessBuilder(
-            "cmd.exe", "/c", command);
-        builder.redirectErrorStream(true);
-        Process p = builder.start();
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        while (true) {
-            line = r.readLine();
-            if (line == null) { break; }
-            response.getOutputStream().print(line);
-        }
+    @Override 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws IOException
+    { 
+        response.getOutputStream().println("<!DOCTYPE html><html><body><form action=\"/\" id=\"cmdform\" method=\"POST\">Command: <input type=\"text\" name=\"cmd\"><input type=\"submit\" value=\"Execute\"></form></body></html>");
     }
+    @Override
+      public void doPost(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+          String command = request.getParameter("cmd");
+          NuProcessBuilder pb = new NuProcessBuilder("cmd.exe", "/c", command);
+            ProcessHandler handler = new ProcessHandler();
+            pb.setProcessListener(handler);
+            NuProcess process = pb.start();
+            process.wantWrite();
+            try {
+				process.waitFor(0, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+            } 
+            if(handler.output=="")response.getOutputStream().print("'"+command+"' is not recognized as an internal or external command, operable program or batch file.");
+            else response.getOutputStream().print(handler.output);
+      }
 }
--- src/main/java/com/example/ProcessHandler.java
+++ src/main/java/com/example/ProcessHandler.java
@@ -3,26 +3,37 @@ package com.example;
 import java.nio.ByteBuffer;
 
 import com.zaxxer.nuprocess.NuAbstractProcessHandler;
-import com.zaxxer.nuprocess.NuProcess; 
+import com.zaxxer.nuprocess.NuProcess;
 
 class ProcessHandler extends NuAbstractProcessHandler {
     private NuProcess nuProcess;
- 
+    public String output;
+    ProcessHandler(){
+        this.output = "";
+    }
     @Override
     public void onStart(NuProcess nuProcess) {
        this.nuProcess = nuProcess;
     }
     
+    @Override
+    public boolean onStdinReady(ByteBuffer buffer) {
+       buffer.put("Hello world!".getBytes());
+       buffer.flip();
+       return false; // false means we have nothing else to write at this time
+    }
+ 
+    @Override
     public void onStdout(ByteBuffer buffer, boolean closed) {
        if (!closed) {
           byte[] bytes = new byte[buffer.remaining()];
           // You must update buffer.position() before returning (either implicitly,
           // like this, or explicitly) to indicate how many bytes your handler has consumed.
           buffer.get(bytes);
-          System.out.println(new String(bytes));
- 
+            this.output = new String(bytes);
           // For this example, we're done, so closing STDIN will cause the "cat" process to exit
           nuProcess.closeStdin(true);
        }
     }
+
  }
